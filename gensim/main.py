6d84b51cf485e6f99cd28dba00f4fc56768c0d9d
Breno <breno@dcc.ufmg.br>
2018-09-26 20:09:19 -0300

    update main.py

--- gensim/main.py
+++ gensim/main.py
@@ -2,22 +2,18 @@ from gensim.models import Word2Vec
 import gensim
 from nltk.tokenize import word_tokenize
 
-# f=open('brasil.txt','r')
-# corpus = f.read()
-# tokenized_text = word_tokenize(corpus)
-# print(tokenized_text)
+corpus = gensim.models.word2vec.Text8Corpus('text8', max_sentence_length=10000)
+model = gensim.models.Word2Vec(corpus,size=200, window=5, min_count=1, workers=5, iter=5,sg=0)
 
-
-#corpus = gensim.models.word2vec.Text8Corpus('text8', max_sentence_length=10000)
-#model = gensim.models.Word2Vec(corpus,size=200, window=5, min_count=1, workers=5, iter=5,sg=0)
-
-#model.save("bin")
-model = gensim.models.KeyedVectors.load_word2vec_format("bin")
+model.save("bin")
 context_words_list = ["mexico", "mexican","macedonia"]
 print(model.predict_output_word(context_words_list, topn=20))
 
+model = gensim.models.Word2Vec.load('bin')
+print(model.wv.accuracy('questions-words_lower.txt'))
+similarities = model.wv.most_similar(positive=['mexico', 'mexican', "macedonia"], topn=20)
+print(similarities)
 
-
-#most_similar_to_given(entity1, entities_list)¶
-#model.accuracy('/tmp/questions-words.txt') esse aqui usa o mesmo questions-words que vem no codigo da google
-https://rare-technologies.com/word2vec-tutorial/
+# esse aqui usa o mesmo questions-words que vem no codigo da google
+# https://rare-technologies.com/word2vec-tutorial/
+# pip install "h5py==2.8.0rc1"
--- gensim/to_lower.py
+++ gensim/to_lower.py
@@ -5,4 +5,4 @@ file = open('questions-words.txt', 'r')
 
 lines = [line.lower() for line in file]
 with open('questions-words_lower.txt', 'w') as out:
-     out.writelines(sorted(lines))
+     out.writelines((lines))
