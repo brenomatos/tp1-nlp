fa93a3f57a0f670ddda7534c56e5d500d3c8413f
Breno <breno@dcc.ufmg.br>
2018-09-27 16:36:02 -0300

    added csv output command

--- gensim/main.py
+++ gensim/main.py
@@ -27,26 +27,36 @@ def treino_word2vec(tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_s
     model.save(output_path)
     return model
 
-def similaridade(input_file, model, output_file):
+def similaridade(input_file, model, output_file, csv_output):
     #palavra0 palavra1 palavra2 palavra3 eh o modelo que tem no questions-words
     #Palavra1 e palavra2 são as positivas. Palavra0 negativa.
+    csv = open(csv_output, "w")
+    csv.write("correct_word,correct_word_similarity,top_minus_correct_similarity\n")
     file = open(input_file, 'r')
-    count = 0
     lines = [line.lower() for line in file]
     for i in lines:
         word_tokens = word_tokenize(i)
         if(word_tokens[0]!=':'): #para ignorar as linhas com dois pontos
             try:
                 similarities = model.wv.most_similar(positive=[word_tokens[1], word_tokens[2]],negative=[word_tokens[0]], topn=50)
-                print(similarities)
+                for i in range(0, len(similarities)):
+                    if (similarities[i][0] == word_tokens[3]):
+                        break
+                # A linha abaixo imprime, nessa ordem: a palavra correta, sua similaridade dadas as 3 outras palavras,
+                # a diferenca entre a com maior similaridade e a similaridade da palavra correta, seguida pelo indice
+                # da palavra correta no top 50
+                csv.write(word_tokens[3]+","+str(similarities[i][1])+","+str(similarities[0][1]-similarities[i][1])+","+str(i)+"\n")
+                # print(word_tokens[3]+","+str(similarities[i][1])+","+str(similarities[0][1]-similarities[i][1])+","+str(i))
             except Exception as e:
-                print(e)
+                pass #ignorando as instancias fora do vocabulario
+    csv.close()
+    file.close()
 
 
 output_path = "w2v-"+str(tng_size)+"-"+str(tng_window)+"-"+str(tng_min_count)+"-"+str(tng_workers)+"-"+str(tng_iter)+"-"+str(tng_sg)
 # model = gensim.models.Word2Vec.load('w2v-200-5-1-5-5-0')
 model = treino_word2vec(tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_sg,output_path)
-similaridade("questions-words.txt", model, output_path)
+similaridade("questions-words.txt", model, output_path, output_path+".csv")
 
 model.wv.accuracy('questions-words.txt')#resulta em estatisticas no log criado
 os.remove(output_path+".trainables.syn1neg.npy")#deletando arquivos desnecessarios
