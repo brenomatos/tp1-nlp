d9617fe5744a906cb3a88f6783f64ef36d8d5618
Breno <breno@dcc.ufmg.br>
2018-09-28 14:57:55 -0300

    plot method now saves all results in a directory

--- gensim/plot.py
+++ gensim/plot.py
@@ -1,24 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import os
 
 def plot(file_path):
-    
+    current_directory = os.getcwd()
+    final_directory = os.path.join(current_directory, r''+str(file_path[:-4])+'-results')
+    if not os.path.exists(final_directory):
+        os.makedirs(final_directory)
+
     df = pd.read_csv(file_path, index_col=False,  escapechar='|', sep=',')
     boxplot = plt.figure()
     plt.boxplot(df["top_minus_correct_similarity"])
     plt.title("Boxplot "+file_path[:-4])
-    boxplot.savefig(file_path[:-4]+".jpg")
+    boxplot.savefig(str(file_path[:-4])+'-results'+"/"+file_path[:-4]+".jpg")
 
-    df.describe().to_csv("boxplot-"+file_path[:-4]+"-"+"results.csv")
+    df.describe().to_csv(str(file_path[:-4])+'-results'+"/"+"boxplot-"+file_path[:-4]+"-"+"results.csv")
 
     scatter = plt.figure()
     plt.scatter(df["correct_word_similarity"], df["top_minus_correct_similarity"])
     plt.title("Scatterplot "+file_path[:-4])
     plt.xlabel("Similaridade - Palavra Correta")
     plt.ylabel("Erro - Similaridade")
-    scatter.savefig("scatter-"+file_path[:-4]+".jpg")
+    scatter.savefig(str(file_path[:-4])+'-results'+"/scatter-"+file_path[:-4]+".jpg")
     # plt.show()
 
 
