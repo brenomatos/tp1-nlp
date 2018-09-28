a71a481b9d35b9261f7dcd34439f43f8c81bbbb9
Breno <breno@dcc.ufmg.br>
2018-09-28 14:18:14 -0300

    added scatterplot method

--- gensim/plot.py
+++ gensim/plot.py
@@ -3,17 +3,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def plot_boxplot(file_path):
+def plot(file_path):
+    
     df = pd.read_csv(file_path, index_col=False,  escapechar='|', sep=',')
-    fig = plt.figure()
+    boxplot = plt.figure()
     plt.boxplot(df["top_minus_correct_similarity"])
-    plt.title("Boxplot")
-    plt.xlabel("eixo x")
-    plt.ylabel("eixo y")
-    fig.savefig(file_path[:-4]+".jpg")
-    df.describe().to_csv(file_path[:-4]+"-"+"results.csv")
+    plt.title("Boxplot "+file_path[:-4])
+    boxplot.savefig(file_path[:-4]+".jpg")
+
+    df.describe().to_csv("boxplot-"+file_path[:-4]+"-"+"results.csv")
+
+    scatter = plt.figure()
+    plt.scatter(df["correct_word_similarity"], df["top_minus_correct_similarity"])
+    plt.title("Scatterplot "+file_path[:-4])
+    plt.xlabel("Similaridade - Palavra Correta")
+    plt.ylabel("Erro - Similaridade")
+    scatter.savefig("scatter-"+file_path[:-4]+".jpg")
+    # plt.show()
 
 
 
-# w2v-200-5-1-5-5-0.csv
-plot_boxplot("w2v-200-5-1-5-5-0.csv")
+plot("w2v-200-5-1-5-5-0.csv")
