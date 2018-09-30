9074cf2233b70afba396dc8396046ff60281713f
breno <breno.ms1997@gmail.com>
2018-09-29 23:20:38 -0300

    added resize_corpus method to to_lower.py

--- gensim/to_lower.py
+++ gensim/to_lower.py
@@ -12,10 +12,23 @@ def to_lower(input_file,output_file):
 
 def resize_corpus(input_file, output_file, percentage):
     with open(input_file, "r") as input:
-        lines = input.read().splitlines()
-        # lines = [for line in file]
-        print(len(lines))
-        # for line in lines:
-            # print(line+"\n\n\n")
+        count = 0
+        str_aux=""
+        list_final = []
+        for line in input:
+            for word in line.split():
+                str_aux = str_aux+word+" "
+                count = count+1
+                if(count==10):
+                    list_final.append(str_aux)
+                    count = 0
+                    str_aux=""
+    with open(output_file, "w") as output:
+        limit = len(list_final)/100*percentage
+        count = 0
+        for line in range(len(list_final)+1):
+            if (count<=limit):
+                output.write(list_final[line])
+                count = count + 1
 
-resize_corpus("text8","lixo",50)
+# resize_corpus("text8","lixo",75)
