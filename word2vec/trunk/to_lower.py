b09946d36fb68d2703306e6fe5bda8b671f47b03
Breno <breno@dcc.ufmg.br>
2018-10-01 15:53:53 -0300

    deleted useless code

--- word2vec/trunk/to_lower.py
+++
@@ -1,8 +0,0 @@
-from itertools import chain
-from glob import glob
-
-file = open('questions-words.txt', 'r')
-
-lines = [line.lower() for line in file]
-with open('questions-words_lower.txt', 'w') as out:
-     out.writelines(sorted(lines))
