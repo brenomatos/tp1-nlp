37238876086bc81ca893a17c3584b770b1d33bf6
Breno <breno.ms1997@gmail.com>
2019-01-12 22:44:11 -0300

    deleted useless files. Translated code comments to english

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tp1-nlp
 code regarding the first assignment of Natural Language Processing class @ UFMG
+
+You may need to run this code snippet if it's the first time you use the nltk library
+```python
+import nltk
+nltk.download('punkt')
+```
+# Corpus used
+matt honey corpus
--- gensim/main.py
+++ gensim/main.py
@@ -8,6 +8,9 @@ import os,logging
 from plot import plot
 from shutil import copyfile
 
+# This method resizes the corpus to a fraction of it's original size. The
+# parameter percentage is used to fix what percentage of the original corpus
+# you'll get on a new file (output_file)
 def resize_corpus(input_file, output_file, percentage):
     with open(input_file, "r") as input:
         count = 0
@@ -29,58 +32,56 @@ def resize_corpus(input_file, output_file, percentage):
                 output.write(list_final[line])
                 count = count + 1
 
-def to_lower(input_file,output_file):#formatando o arquivo de validacao para minusculo
+def to_lower(input_file,output_file):# Formatting the "questions-words.txt" to lowercase
     file = open(input_file, 'r')
     lines = [line.lower() for line in file]
     with open(output_file, 'w') as out:
          out.writelines((lines))
 
-def treino_word2vec(corpus_file,tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_sg,output_path):
+def return_word2vec_model(corpus_file,tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_sg,output_path):
     corpus = gensim.models.word2vec.Text8Corpus(corpus_file, max_sentence_length=10000)
     model = gensim.models.Word2Vec(corpus,size=tng_size, window=tng_window, min_count=tng_min_count, workers=tng_workers, iter=tng_iter,sg=tng_sg)
     model.save(output_path)
     return model
 
-def similaridade(input_file, model, output_file, csv_output):
-    #palavra0 palavra1 palavra2 palavra3 eh o modelo que tem no questions-words
-    #Palavra1 e palavra2 são as positivas. Palavra0 negativa.
+def similarity(input_file, model, output_file, csv_output):
+    #word0 word1 word2 word3 is the template used in "questions-words.txt"
+    #word1 and word2 are "positive". word0 is "negative".
     csv = open(csv_output, "w")
     csv.write("correct_word,correct_word_similarity,top_minus_correct_similarity,similarity_index\n")
     file = open(input_file, 'r')
     lines = [line.lower() for line in file]
     for i in lines:
         word_tokens = word_tokenize(i)
-        if(word_tokens[0]!=':'): #para ignorar as linhas com dois pontos
+        if(word_tokens[0]!=':'): # ignores the lines with ":"
             try:
                 similarities = model.wv.most_similar(positive=[word_tokens[1], word_tokens[2]],negative=[word_tokens[0]], topn=50)
                 for i in range(0, len(similarities)):
                     if (similarities[i][0] == word_tokens[3]):
                         break
-                # A linha abaixo imprime, nessa ordem: a palavra correta, sua similaridade dadas as 3 outras palavras,
-                # a diferenca entre a com maior similaridade e a similaridade da palavra correta, seguida pelo indice
-                # da palavra correta no top 50
+                # The line below prints, in this order: the correct word, it's similarity given the other 3 words,
+                # the diference between the top ranking word's similarity and the similarity of the correct word,
+                # followed by the index of the correct word in the top 50 suggested words
                 csv.write(word_tokens[3]+","+str(similarities[i][1])+","+str(similarities[0][1]-similarities[i][1])+","+str(i)+"\n")
-                # print(word_tokens[3]+","+str(similarities[i][1])+","+str(similarities[0][1]-similarities[i][1])+","+str(i))
+
             except Exception as e:
-                pass #ignorando as instancias fora do vocabulario
+                pass # Ignore "out of vocabulary" instances
     csv.close()
     file.close()
 
-# A main trata o arquivo questions-words, treina o modelo, testa acuracia e gera graficos
+# the main method preprocesses the questions-words file, trains the model, tests it's accuracy e generates graphs
 def main(corpus_file,tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_sg):
-    to_lower("questions-words.txt","questions-words.txt")#alterando arquivo para minusculo por garantia
+    to_lower("questions-words.txt","questions-words.txt")# altering the file to lowercase, just to be sure
     output_path = corpus_file +"-w2v-"+str(tng_size)+"-"+str(tng_window)+"-"+str(tng_min_count)+"-"+str(tng_workers)+"-"+str(tng_iter)+"-"+str(tng_sg)
     logging.basicConfig(filename="logTotal.log",format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO, filemode="w")
 
-    # model = gensim.models.Word2Vec.load('w2v-200-5-1-5-5-0')
-    model = treino_word2vec(corpus_file,tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_sg,output_path)
-    similaridade("questions-words.txt", model, output_path, output_path+".csv")
+    model = return_word2vec_model(corpus_file,tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_sg,output_path)
+    similarity("questions-words.txt", model, output_path, output_path+".csv")
 
-    model.wv.accuracy('questions-words.txt')#resulta em estatisticas no log criado
-    os.remove(output_path+".trainables.syn1neg.npy")#deletando arquivos desnecessarios
+    model.wv.accuracy('questions-words.txt')# creates statistics in the log file
+    os.remove(output_path+".trainables.syn1neg.npy")# deletes unnecessary files
     os.remove(output_path+".wv.vectors.npy")
     plot(output_path+".csv")
-    # logging.disable(level=1)
 
 tng_size = 200
 # tng_window = 5
@@ -90,11 +91,12 @@ tng_iter = 5
 # tng_sg = 0
 
 current_directory = os.getcwd()
-copyfile(current_directory+"/text8", current_directory+"/corpus100")# apenas por padronizacao
+copyfile(current_directory+"/text8", current_directory+"/corpus100")# keeps the same pattern
 resize_corpus("text8","corpus75",75)
 resize_corpus("text8","corpus50",50)
 
 for corpus_file in range(50,125,25):
     for tng_window in range(2, 12, 2):
         for tng_sg in range(0,2):
+            print(str(corpus_file)+"-"+str(tng_window)+"-"+str(tng_sg))
             main("corpus"+str(corpus_file),tng_size,tng_window,tng_min_count,tng_workers,tng_iter,tng_sg)
